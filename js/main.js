995bbda7ca7161f434d94a4b6b1cc33f04d48037
AlinaKurochka <alinayaremachenko@gmail.com>
2017-12-03 18:06:42 +0200

    added 8 button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 $( document ).ready(function() {
     $('.js-btn-1').click( function() {
-    	$( this ).addClass('btn-primary');
+    	$(this).addClass('btn-primary');
     });
-    $('.js-btn-2').click( function(){
+    $('.js-btn-2').click( function() {
     	$(this).removeClass('btn-secondary');
     });
-});
+    $('.js-btn-3').click( function() {
+    	$(this).toggleClass('btn-success');
+    });
+    $('.js-btn-4').click( function() {
+    	console.log($(this).attr('class'));
+    });
+    $('.js-btn-5').click( function() {
+    	$(this).attr('disabled', 'disabled');
+    });
+    $('.js-btn-6').click( function() {
+    	alert("Hello World!");
+    });
+	$('.js-btn-7').bind('click', function(){
+	  alert('Шла Саша по шоссе');
+	});
+	$('.js-btn-7').trigger('click');
+	});
