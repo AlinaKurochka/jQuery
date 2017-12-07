9792ec3a6014594a63157f4c19ff93051b9a69fd
AlinaKurochka <alinayaremachenko@gmail.com>
2017-12-07 20:57:44 +0200

    task#2

--- js/main2.js
+++ js/main2.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
-	
+	$('select,input, button').on('change',function(){
+		console.log($(this).val());
+	})
+	$('#form').submit(function(event){
+		event.preventDefault();
+		console.log($(this).serialize());
+	});
+	$('#username1').on('keyup', function(){
+		var value = $(this).val();
+		$('#username2').val(value);
+	})
 });
